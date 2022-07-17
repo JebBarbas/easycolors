2d7d3ada65baa2986b3d3d011725e081bcbd9044
Enrique Barboza <enriquebarbozajeb@gmail.com>
2022-07-16 19:53:36 -0700

    First commit: Nombre apartado

---
+++ tests/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line no-undef
+const { ok } = require('node:assert')
+const { log } = console
+
+log('Ya quedó, muy bonito')
+ok(true)
